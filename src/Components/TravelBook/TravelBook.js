3e7106ea24591a74c1477c8c926e0f96f5ca7ccb
Destiny Jones <destinyjones@Destinys-MacBook.local>
2019-06-14 16:52:59 -0700

    created a api request that works on :)

--- src/Components/TravelBook/TravelBook.js
+++ src/Components/TravelBook/TravelBook.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
+import { setUserDestination } from '../../ducks/travelReducer';
 import './TravelBook.scss';
 import HillSide from '../../images/HillSide.jpeg';
 
 
-
+const { REACT_APP_API_KEY } = process.env
 
 class TravelBook extends Component {
     constructor(props){
         super(props) 
         this.state = {
-            locations: []
+            locations: [],
+            userInput: ""
         }
     }
 
     componentDidMount(){
         this.getAllLocations()
+    }
+
+    handleChange(value){
+        this.setState({
+            userInput: value
+        })
+    }
+
+    getData = () => {
+        axios.get(`https://api.sygictravelapi.com/1.1/en/places/list?limit=10&query=${this.state.userInput}`, {
+        headers: {
+           "x-api-key":  REACT_APP_API_KEY
+        }
+       }).then(res => {
+            console.log('got response')
+           this.props.setUserDestination({
+            userInput: res.data.data.places
+           })
+           console.log(res.data.data.places)
+       })
     }
 
     getAllLocations() {
@@ -24,13 +47,19 @@ class TravelBook extends Component {
             locations: res.data
         })
         console.log(res.data)
-    }).catch(error => console.log(error))
+    }).catch(error => console.log('GET ALL',error))
     }
     render(){
       const mappedgetAllLocations = this.state.locations.map( locations => (
           <div key={locations.location_id}>
-              <h2>{locations.name}</h2>
-              <img src={locations.image_url} alt='location'/>
+              <div className="popular-box">
+                <img className="card" src={locations.image_url} alt='location'/>
+                <div className="card-info">
+                     <h2 className="card-name">{locations.name}</h2>
+                     <button className="card-button">Add</button>
+                     <p className="card-p">{locations.description}</p>
+                </div>
+              </div>
           </div>
       ))
     return (
@@ -44,21 +73,24 @@ class TravelBook extends Component {
                 <h3>Choose Your Destination</h3>
                 <hr/>
             <div className="search-bar">
-                    <input placeholder="Destination"/>
+                    <input placeholder="Destination" onChange={(e) => this.handleChange(e.target.value)}/>
                 
-                    <select>
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="mercedes">Mercedes</option>
-                        <option value="audi">Audi</option>
-                    </select>
+                    {/* <select>
+                        <option value="shopping">Shopping</option>
+                        <option value="eating">Resturants</option>
+                        <option value="sports">Sports</option>
+                        <option value="discovering">Discoveries</option>
+                        <option value="going_out">NightLife</option>
+                        <option value="siteseeing">Siteseeing</option>
+                    </select> */}
             
-                    <button>Search</button>
+                    <button id="search" onClick={this.getData}>Search</button>
             </div>
         </div>
     </div> 
 
-        <h1>Explore </h1>
+        <h1 className="popular-section">Popular Destination</h1>
+         <hr className="popular-line"/>
         <div>{mappedgetAllLocations}</div>
         
     </div>
@@ -66,7 +98,19 @@ class TravelBook extends Component {
     )}
 
 }
-    export default TravelBook
+
+const mapStateToProps = (reduxState) => {
+    return reduxState
+}
+const mapDispatchToProps = {
+    setUserDestination 
+}
+
+const invokedConnect = connect(
+    mapStateToProps,
+    mapDispatchToProps 
+)
+    export default invokedConnect(TravelBook)
 
 
 
--- src/ducks/travelReducer.js
+++ src/ducks/travelReducer.js
@@ -1,33 +1,29 @@
 
 const initalState = {
-    places: { }
+    userDestination: { }
 }
 
 
-const FETCH_ITEMS = "FETCH_ITEMS"
+const FETCH_DESTINATIONS = "FETCH_DESTINATIONS"
 
 export default function (state = initalState, action){
     switch(action.type){
-        case FETCH_ITEMS:
+        case FETCH_DESTINATIONS:
             return {
                 ...state,
-                places: action.payload
+                userDestination: action.payload
             }
         default:
             return state;
     }
 }
 
-// export  const fetchItems = () => (dispatch) => {
-//     console.log('fetching')
-//     fetch('https://api.sygictravelapi.com/1.1/en/places/list')
-//     .then(res => res.json())
-//     .then(places => dispatch({
-//         type: FETCH_ITEMS,
-//         payload: places
-//     }));
-    
-// }
 
+export function setUserDestination(userDestination){
+    return {
+        type: FETCH_DESTINATIONS,
+        payload: userDestination
+    }
+}
 
 
